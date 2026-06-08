b82f10e902af965e0460adacbdecf26eb1929b8a
Domas <domas@athomos.net>
2026-06-08 12:04:34 +0200

    chore: use dedicated booking_app_test DB for tests; enable loyalty in SystemSettingSeeder

--- database/seeders/SystemSettingSeeder.php
+++ database/seeders/SystemSettingSeeder.php
@@ -10,9 +10,13 @@ class SystemSettingSeeder extends Seeder
     public function run(): void
     {
         $defaults = [
-            'slot_generation_weeks'    => 4,
-            'slot_granularity_minutes' => 10,
-            'timezone'                 => 'Europe/Rome',
+            'slot_generation_weeks'       => 4,
+            'slot_granularity_minutes'    => 10,
+            'timezone'                    => 'Europe/Rome',
+            'loyalty_enabled'             => true,
+            'loyalty_points_per_euro'     => 1,
+            'loyalty_reward_threshold'    => 100,
+            'loyalty_reward_percentage'   => 10,
         ];
 
         $setting = SystemSetting::first();
