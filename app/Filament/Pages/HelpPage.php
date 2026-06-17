7bdc0f41d5ab267f47fcb2304f94dc3fcd8fab3a
Domas <domas@athomos.net>
2026-06-17 14:57:24 +0200

    feat: add integration status to HelpPage

--- app/Filament/Pages/HelpPage.php
+++ app/Filament/Pages/HelpPage.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Pages;
 
+use App\Models\IntegrationSetting;
 use Filament\Pages\Page;
 
 class HelpPage extends Page
@@ -12,6 +13,31 @@ class HelpPage extends Page
     protected static ?int $navigationSort = 100;
 
     protected string $view = 'filament.pages.help';
+
+    public array $integrationStatuses = [];
+    public string $integrationSettingsUrl = '';
+
+    public function mount(): void
+    {
+        $setting = IntegrationSetting::current();
+
+        $this->integrationSettingsUrl = IntegrationSettings::getUrl();
+
+        $this->integrationStatuses = [
+            'stripe' => [
+                'label'      => 'Stripe',
+                'configured' => ! empty($setting->stripe_public_key) && ! empty($setting->stripe_secret_key),
+            ],
+            'whatsapp' => [
+                'label'      => 'WhatsApp',
+                'configured' => IntegrationSetting::hasMetaWhatsApp(),
+            ],
+            'google_calendar' => [
+                'label'      => 'Google Calendar',
+                'configured' => ! empty($setting->google_calendar_id) && ! empty($setting->google_credentials_json),
+            ],
+        ];
+    }
 
     public static function canAccess(): bool
     {
